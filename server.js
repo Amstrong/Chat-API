7c9b9c8954024669262ce26ef62b9c2961066ae8
Amstrong <monachelloa@uninorte.edu.co>
2020-08-29 02:22:07 -0500

    Agregando cabeceras personalizadas

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
+//Es una extensión un modulo de express que nos ayuda a trbajar con
+//la petición de una forma sencilla.
+const bodyParser = require("body-parser");
+const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
-// Aquí la aplicación estara siempre escuchando y nos 
+// Aquí la aplicación estara siempre escuchando y nos
 //devolverá un hola.
-app.use("/", function (req, res) {
-  res.send("Hola");
+// app.use("/", function (req, res) {
+//   res.send("Hola");
+// });
+
+router.get("/message", function (req, res) {
+  console.log(req.headers)
+  res.header({
+    "custom-header":"Nuestro valor personalizado"
+  })
+  res.send("Lista de mensajes");
+});
+
+router.delete("/message", function (req, res) {
+  console.log(req.query);
+  res.send("Mensaje:" + req.query.text + "añadido");
 });
 
 app.listen(3000);
-console.log("La app está escuchando en Http://Localhost:3000/")
+console.log("La app está escuchando en http://Localhost:3000/");
