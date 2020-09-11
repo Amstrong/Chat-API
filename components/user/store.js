84a4eb2e8dd5c166a06ce9bf8a1d1f6bbd9967ed
Amstrong <monachelloa@uninorte.edu.co>
2020-09-11 01:02:12 -0500

    Escalando la arquitectura: Múltiples entidades

---
+++ components/user/store.js
@@ -0,0 +1,9 @@
+const Model = require("./model");
+
+function addUser(user) {
+  const myUser = new Model(user);
+ return myUser.save();
+}
+module.exports = {
+  add: addUser,
+};
--- network/routes.js
+++ network/routes.js
@@ -1,6 +1,9 @@
-const express = require ("express")
-const message = require("../components/message/network")
+const express = require("express");
+const message = require("../components/message/network");
+const user = require("../components/user/network");
+
 const routes = function (server) {
-server.use("/message", message)
-}
+  server.use("/message", message);
+  server.use("/user", user);
+};
 module.exports = routes;
