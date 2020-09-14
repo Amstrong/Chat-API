706a2e8ebe71e932b7ddf3d497e17e70d1813873
Amstrong <monachelloa@uninorte.edu.co>
2020-09-13 23:26:55 -0500

    Refactorizando

--- network/routes.js
+++ network/routes.js
@@ -3,6 +3,8 @@ const message = require("../components/message/network");
 const user = require("../components/user/network");
 const chat = require("../components/chat/network");
 
+
+
 const routes = function (server) {
   server.use("/message", message);
   server.use("/user", user);
